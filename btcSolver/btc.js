f4483dfcbbaf862539c6fc3c284ea8c34ce76ada
sayine <83287127+sayine@users.noreply.github.com>
2024-06-03 13:05:39 +0300

    Update btc.js

--- btcSolver/btc.js
+++ btcSolver/btc.js
@@ -1,109 +1,48 @@
-const bitcoin = require("bitcoinjs-lib");
-const ec = require("elliptic").ec;
-const sha256 = require('js-sha256');
-const ripemd160 = require('ripemd160');
-const base58 = require('bs58');
-
+const fs = require('node:fs');
+var CoinKey = require('coinkey');
+const BigNumber = require('bignumber.js');
 var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'nodejs577@gmail.com',
-      pass: 'khve hbqo fdgv ygdh'
-    }
-  });
+var transporter = nodemailer.createTransport({service: 'gmail',auth: {user: 'nodejs577@gmail.com',pass: 'khve hbqo fdgv ygdh'}});
 
 let Bitcoin = {};
+var i = new BigNumber(0);
+var key65M = new BigNumber('35175b6d07a107907', 16); // 273d906442a699f61 // 3a98787a0ac00417a // 35e982ee63941ee34 // 390137cfea9877cf7
+// 3595af90f7f6e3468 34803c03164f5d43c 2f18fe5b7e556b3a0 21ef17473caaacc00 363d541eb35e4bbe0
+var counter = 0;
 do {
+var key66M = key65M.plus(i);
+var p66HexNumberM = key66M.toString(16) // .slice(0, 29); 
+var addressM = 0;
+var privateKeyM = 0;
+var remain = counter % 160000;
 
-function getRN() {
-  let arrayRN = new Uint8Array(1);
-  crypto.getRandomValues(arrayRN);
-  return arrayRN[0] % 16;
-}
-
-var list = ["a","b","c","d","e","f",0,1,2,3,4,5,6,7,8,9];
-var pK = '00000000000000000000000000000000000000';
-var pKArray = [...pK];
-
-for (let i = 0; i < pKArray.length; i++) {
-  pKArray[i] = list[getRN()];
-}
-
-//console.log(pKArray.join(''));
-
-var address = 0;
-var privateKey = 0;
-// Generate the key and address
-Bitcoin.createWalletAddress = (callback) => {
-    let privateKey = Bitcoin.createPrivateKey();
-    let hash = Bitcoin.generatePublicKeyHash(privateKey);
-    address = Bitcoin.createPublicAddress(hash);
-
-    console.log(privateKey);
-    console.log(address);
-}
-// 1NBC8uXJy1GiJ6drkiZa1WuKn51ps7EPTv
-// Create the private key 
 Bitcoin.createPrivateKey = () => {
-    var keyPair = '00000000000000000000000001' + pKArray.join('');
-    privateKey = keyPair.toString('hex');
-    return privateKey;
+    var keyPairM = '00000000000000000000000000000000000000000000000' + p66HexNumberM;
+    privateKeyM = keyPairM.toString('hex');
+    var keyM = new CoinKey(new Buffer.from(privateKeyM, 'hex'));
+    keyM.compressed = true;
+    addressM = keyM.publicAddress;
+    wifM = keyM.privateWif;
+
+    if(remain == 0) {
+      console.log('M:', privateKeyM);}
 }
+  counter += 1;
+  Bitcoin.createPrivateKey();
 
-/**
- * Generate the public key hash
- * 
- * @param {String} privateKey 
- */
-Bitcoin.generatePublicKeyHash = privateKey => {
-    const ecdsa = new ec('secp256k1'),
-        keys = ecdsa.keyFromPrivate(privateKey),
-        publicKey = keys.getPublic('hex'),
-        hash = sha256(Buffer.from(publicKey, 'hex')),
-        publicKeyHash = new ripemd160().update(Buffer.from(hash, 'hex')).digest();
+  if (addressM === '13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so') {
+    var mailOptions = {from: 'nodejs577@gmail.com',to: 'emrahsayin@yandex.com',subject: privateKeyM,text: wifM};
+    transporter.sendMail(mailOptions, function(error, info){if (error) {console.log(error);} else {console.log('Email sent: ' + info.response);}});
+    fs.appendFile('address.json', wifM, 'utf8', function (err) {
+      if (err) {
+          return console.log(err);
+      }
+  
+      console.log("The file was saved!");
+  }); 
+  };
 
-    return publicKeyHash;
+  i = i.minus(1);
 }
-
-/**
- * Create a public address based on the hash
- * 
- * @param {String} publicKeyHash 
- */
-Bitcoin.createPublicAddress = publicKeyHash => {
-    // step 1 - add prefix "00" in hex
-    const step1 = Buffer.from("00" + publicKeyHash.toString('hex'), 'hex');
-    // step 2 - create SHA256 hash of step 1
-    const step2 = sha256(step1);
-    // step 3 - create SHA256 hash of step 2
-    const step3 = sha256(Buffer.from(step2, 'hex'));
-    // step 4 - find the 1st byte of step 3 - save as "checksum"
-    const checksum = step3.substring(0, 8);
-    // step 5 - add step 1 + checksum
-    const step4 = step1.toString('hex') + checksum;
-    // return base 58 encoding of step 5
-    const address = base58.encode(Buffer.from(step4, 'hex'));
-    return address;
-  }
-
-  Bitcoin.createWalletAddress();
-
-  if (address === '18192XpzzdDi2K11QVHR7td2HcPS6Qs5vg') {
-    var mailOptions = {
-  from: 'nodejs577@gmail.com',
-  to: 'sayinemrah@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: privateKey
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-  };}
-  while(address !== '18192XpzzdDi2K11QVHR7td2HcPS6Qs5vg');
+  while(addressM !== '13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so');
